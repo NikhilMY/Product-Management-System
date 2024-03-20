f50c0967358cfe9417c51c975f850ef87a880116
NikhilMY <nikhilmy09@gmail.com>
2024-03-20 22:16:45 +0530

    Product

---
+++ src/main/java/com/jsp/product/model/Product.java
@@ -0,0 +1,39 @@
+package com.jsp.product.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pId;
+	private String pName;
+	private int pCost;
+	public int getpId() {
+		return pId;
+	}
+	public void setpId(int pId) {
+		this.pId = pId;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public int getpCost() {
+		return pCost;
+	}
+	public void setpCost(int pCost) {
+		this.pCost = pCost;
+	}
+	@Override
+	public String toString() {
+		return "Product [pId=" + pId + ", pName=" + pName + ", pCost=" + pCost + "]";
+	}
+	
+
+}
