5d0af8368e88acc74b27e1cc4ccae19ca1ad5670
NikhilMY <nikhilmy09@gmail.com>
2024-03-20 22:23:16 +0530

    ResponseStructure

---
+++ src/main/java/com/jsp/product/utility/ResponseStructure.java
@@ -0,0 +1,31 @@
+package com.jsp.product.utility;
+
+public class ResponseStructure<T> {
+	private int statuscode;
+	private String message;
+	private T data;
+
+	public int getStatuscode() {
+		return statuscode;
+	}
+	public ResponseStructure<T> setStatuscode(int statuscode) { //method chaining happing bcz the returntype is ResponseStructure
+		this.statuscode = statuscode;
+		return this;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public ResponseStructure<T> setMessage(String message) {
+		this.message = message;
+		return this;
+	}
+	public T getData() {
+		return data;
+	}
+	public ResponseStructure<T> setData(T data) {
+		this.data = data;
+		return this;
+	}
+	
+	
+}
